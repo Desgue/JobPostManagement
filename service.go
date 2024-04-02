e21c9031a95198e20a6bf6a4b4c8d535f0e0de89
Desgue <guedesgomero@gmail.com>
2024-04-02 12:44:53 +0100

    implement s3 service

--- service.go
+++ service.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -34,6 +35,14 @@ func NewJobService(jobStore *JobStore, s3Client *s3.Client) *JobService {
 }
 
 func (s *JobService) GetFeed() ([]Job, error) {
+	var feed []Job
+	output, err := ReadFileFromBucket("JobPostings", s.s3Client, "published_jobs.json")
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(output, &feed); err != nil {
+		return nil, err
+	}
 	return s.JobStore.GetFeed()
 }
 
